9e0474418db8abef33843f54a66effce91fd8344
Ivan Tvisher <Tvisher@mail.ru>
2024-10-23 18:44:21 +0500

    some fix

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ cardsSliders.forEach(wrapper => {
     const prevSlideBtn = wrapper.querySelector('.swiper-button-prev');
     const nextSlideBtn = wrapper.querySelector('.swiper-button-next');
     const cardsSlider = new Swiper(sliderElement, {
-        slidesPerView: 1,
+        slidesPerView: 'auto',
         spaceBetween: 10,
         navigation: {
             nextEl: nextSlideBtn,
@@ -14,11 +14,9 @@ cardsSliders.forEach(wrapper => {
         },
         breakpoints: {
             768: {
-                slidesPerView: 2,
                 spaceBetween: 20
             },
             992: {
-                slidesPerView: 'auto',
                 spaceBetween: 40
             }
         }
