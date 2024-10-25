e8a940f6118be93e534746dcfdc72c530c77ab6f
Ivan Tvisher <Tvisher@mail.ru>
2024-10-25 16:03:51 +0500

    +_

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
 
+$(document).ready(function () {
+    $('.has-submenu > a').click(function (e) {
+        e.preventDefault();
+        var $submenu = $(this).next('.submenu');
+
+        // Закрыть все открытые подменю, если кликнули на другой пункт меню
+        $('.submenu').not($submenu).slideUp();
+        $('.has-submenu').not($(this).parent()).removeClass('active');
+
+        // Переключить текущее подменю
+        $(this).parent().toggleClass('active');
+        $submenu.slideToggle();
+    });
+});
+
 
 const cardsSliders = document.querySelectorAll('.cards-section__wrapper');
 cardsSliders.forEach(wrapper => {
@@ -22,3 +37,4 @@ cardsSliders.forEach(wrapper => {
         }
     });
 })
+
