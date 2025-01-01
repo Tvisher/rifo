32e51454dcb06709e68a617cd619605e0efe2e82
Ivan Tvisher <Tvisher@mail.ru>
2025-01-01 20:39:42 +0500

    +

--- js/main.js
+++ js/main.js
@@ -55,3 +55,50 @@ const articleSlider = new Swiper('.article-page__slider', {
         prevEl: '.swiper-button-prev',
     },
 })
+
+const mainImageSvg = document.querySelector('#main-image');
+const mainImageLayers = [...mainImageSvg.querySelectorAll('[data-main-layer]')];
+const selectActiveLayer = (ind) => {
+    mainImageLayers.forEach(el => el.classList.remove('active'));
+    let activeLayer = mainImageLayers.find(el => el.dataset.mainLayer == ind);
+    activeLayer && activeLayer.classList.add('active');
+    mainImageSvg.setAttribute('data-active-index', ind);
+}
+
+let activeMainSlide = 0;
+const mainImageSlider = new Swiper('.main-image-slider', {
+    slidesPerView: 'auto',
+    speed: 400,
+    spaceBetween: 20,
+    loop: 1,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: true,
+    },
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    on: {
+        activeIndexChange: (slider) => {
+            const activeSlideIndex = slider.realIndex + 1
+            activeMainSlide = activeSlideIndex;
+            console.log(activeSlideIndex);
+            selectActiveLayer(activeSlideIndex);
+        },
+    },
+
+});
+
+mainImageLayers.forEach(el => {
+    el.addEventListener('click', (e) => {
+        const elIndex = el.dataset.mainLayer;
+        selectActiveLayer(elIndex);
+        mainImageSlider.slideTo(elIndex - 1);
+        mainImageSvg.classList.add('selected')
+    });
+})
