5ac24275e4ece9f73c8e59f4e80d16e4d75e2602
Ivan Tvisher <Tvisher@mail.ru>
2025-01-09 11:16:20 +0500

    +

--- js/main.js
+++ js/main.js
@@ -63,10 +63,30 @@ const layersBackBtn = document.querySelector('.layers-back')
 
 const mainImageLayers = [...mainImageSvg.querySelectorAll('[data-main-layer]')];
 const selectActiveLayer = (ind) => {
+    const prevActiveInnerLayer = document.querySelector('[data-layer].show');
+    if (prevActiveInnerLayer) prevActiveInnerLayer.classList.remove('show');
+
     mainImageLayers.forEach(el => el.classList.remove('active'));
     let activeLayer = mainImageLayers.find(el => el.dataset.mainLayer == ind);
     activeLayer && activeLayer.classList.add('active');
     mainImageSvg.setAttribute('data-active-index', ind);
+
+    const activeInnerLayer = document.querySelector(`[data-layer="${ind}"]`);
+    if (activeInnerLayer && mainImageSection.classList.contains('selected')) {
+        activeInnerLayer.classList.add('show');
+        const layerIndex = activeInnerLayer.getAttribute('data-layer');
+        const layerElems = activeInnerLayer.querySelectorAll('[data-inner]');
+
+        const activeElemInLayer = layerElems[0];
+        const activeElemInLayerDataId = activeElemInLayer.getAttribute('data-inner');
+
+        activeElemInLayer.classList.add('active');
+        const activeLayerElemsInImage = document.querySelectorAll(`[data-layer-elem = '${layerIndex}-${activeElemInLayerDataId}']`);
+        console.log(activeLayerElemsInImage);
+        activeLayerElemsInImage.forEach(el => el.classList.add('active'));
+
+
+    }
 }
 
 let activeMainSlide = 0;
@@ -84,14 +104,15 @@ const mainImageSlider = new Swiper('.main-image-slider', {
         crossFade: true
     },
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: '.main-image-slider__wrapper .swiper-button-next',
+        prevEl: '.main-image-slider__wrapper .swiper-button-prev',
     },
     on: {
         activeIndexChange: (slider) => {
-            const activeSlideIndex = slider.realIndex + 1
+            const activeSlideIndex = slider.realIndex + 1;
+            // if (activeMainSlide == activeSlideIndex) return;
             activeMainSlide = activeSlideIndex;
-            console.log(activeSlideIndex);
+            cleanAllActivePath();
             selectActiveLayer(activeSlideIndex);
         },
     },
@@ -102,13 +123,41 @@ mainImageLayers.forEach(el => {
     el.addEventListener('click', (e) => {
         mainImageSlider.autoplay.pause();
         const elIndex = el.dataset.mainLayer;
-        selectActiveLayer(elIndex);
-        mainImageSlider.slideTo(elIndex - 1);
+        // selectActiveLayer(elIndex);
         mainImageSection.classList.add('selected');
+        mainImageSlider.slideTo(elIndex - 1);
     });
 });
 
 layersBackBtn.addEventListener('click', () => {
+    cleanAllActivePath();
+    cleanAllActiveListElems();
     mainImageSection.classList.remove('selected');
     mainImageSlider.autoplay.start();
+    const prevActiveInnerLayer = document.querySelector('[data-layer].show');
+    if (prevActiveInnerLayer) prevActiveInnerLayer.classList.remove('show');
 });
+
+function cleanAllActivePath() {
+    const activePathList = mainImageSection.querySelectorAll('[data-layer-elem].active');
+    activePathList.forEach(el => el.classList.remove('active'));
+}
+
+function cleanAllActiveListElems() {
+    const activeListElems = mainImageSection.querySelectorAll('[data-inner].active');
+    activeListElems.forEach(el => el.classList.remove('active'));
+}
+
+
+const layerInnerItems = document.querySelectorAll('[data-inner]');
+layerInnerItems.forEach(el => {
+    const elParentId = el.closest('[data-layer]').getAttribute('data-layer');
+    el.addEventListener('click', () => {
+        cleanAllActivePath();
+        cleanAllActiveListElems();
+        el.classList.add('active');
+        const elId = el.getAttribute('data-inner');
+        const activeLayerElemsInImage = document.querySelectorAll(`[data-layer-elem = '${elParentId}-${elId}']`);
+        activeLayerElemsInImage.forEach(el => el.classList.add('active'));
+    });
+})
